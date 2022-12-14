784adf9c85d0d24df3b82f264d461ce32f67d328
jpmorrissey11 <112785073+jpmorrissey11@users.noreply.github.com>
2022-12-13 17:55:46 -0800

    Update summary_data_generator.py

--- summarization-t5/summary_data_generator.py
+++ summarization-t5/summary_data_generator.py
@@ -4,35 +4,7 @@ import pandas as pd
 import html
 import pandas as pd
 from transformers import pipeline
-
-
-def replace_html(text):
-    text = re.sub("<.*?>", " ", text)
-    text = re.sub("\(.*?\)", " ", text)
-    text = re.sub(r"http\S+", "", text)
-    return text.strip()
-
-
-def clean(text):
-    # convert html escapes like & to characters.
-    text = html.unescape(text)
-    # tags like
-    text = re.sub(r"<[^<>]*>", " ", text)
-    # markdown URLs like [Some text](https://....)
-    text = re.sub(r"\[([^\[\]]*)\]\([^\(\)]*\)", r"\1", text)
-    # text or code in brackets like [0]
-    text = re.sub(r"\[[^\[\]]*\]", " ", text)
-    # standalone sequences of specials, matches  but not #cool
-    text = re.sub(r"(?:^|\s)[<>{}\[\]+|\\:-]{1,}(?:\s|$)", " ", text)
-    # standalone sequences of hyphens like --- or ==
-    text = re.sub(r"(?:^|\s)[\-=\+]{2,}(?:\s|$)", " ", text)
-    # sequences of white spaces
-    text = re.sub(r"\s+", " ", text)
-    text = re.sub(r"[0-9]{2}:[0-9]{2}:[0-9]{2}", "", text)
-    text = re.sub(r"http\S+", "", text)
-    text = re.sub(r"^https?:\/\/.*[\r\n]*", "", text, flags=re.MULTILINE)
-
-    return text.strip()
+from utility import replace_html, text
 
 
 def get_summaries(text, summarizer=s):
@@ -43,7 +15,7 @@ def get_summaries(text, summarizer=s):
         return "too many tokens"
 
 
-path = "/content/drive/MyDrive/Data/First Year 2022 _1500 Units Accounting DAS.csv"
+path = "path/to/data"
 df = pd.read_csv(path)
 df.columns = [c.lower() for c in df.columns]
 
